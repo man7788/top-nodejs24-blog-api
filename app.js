a9aae043880227f5d2a91f718b12d3577068e25d
man7788 <jerman1841@gmail.com>
2025-08-27 02:31:29 +0800

    create express app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use('/', routes.auth);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}!`);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
