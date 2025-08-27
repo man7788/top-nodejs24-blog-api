050399e021efdea01f6d9f3e2f4cd02e053e31b2
man7788 <jerman1841@gmail.com>
2025-08-28 07:42:44 +0800

    create passport config

---
+++ config/passport.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const db = require('../database/userQuery');
+
+const publicKey = Buffer.from(process.env.JWT_PUB_KEY, 'base64').toString(
+  'ascii',
+);
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: publicKey,
+};
+
+const verifyCallback = async (jwt_payload, done) => {
+  try {
+    const user = await db.readUserById(jwt_payload.sub);
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    return done(null, user);
+  } catch (err) {
+    return done(err, false);
+  }
+};
+
+passport.use(new JwtStrategy(options, verifyCallback));
