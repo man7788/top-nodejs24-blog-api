a4195e5af14ff2bc523232d963fb7776dbfd4947
man7788 <jerman1841@gmail.com>
2025-08-29 05:21:49 +0800

    update post router

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -5,30 +5,35 @@ const postController = require('../controllers/postController');
 
 const postRouter = Router();
 
+// POST request to create new post
 postRouter.post(
   '/',
   passport.authenticate('jwt', { session: false }),
   postController.postBlogPost
 );
 
+// GET request to get all posts
 postRouter.get(
   '/',
   passport.authenticate('jwt', { session: false }),
   postController.getAllPosts
 );
 
+// GET request to get a single post
 postRouter.get(
   '/:postId',
   passport.authenticate('jwt', { session: false }),
   postController.getPost
 );
 
+// Patch request to update a single post
 postRouter.patch(
   '/:postId',
   passport.authenticate('jwt', { session: false }),
   postController.patchPost
 );
 
+// Delete request to delete a single post
 postRouter.delete(
   '/:postId',
   passport.authenticate('jwt', { session: false }),
