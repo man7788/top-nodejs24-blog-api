6fe07e1fab3df90b72704985b4891d5f122ecc4c
man7788 <jerman1841@gmail.com>
2025-08-29 05:21:35 +0800

    update auth router

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/authController');
 
 const authRouter = Router();
 
+// POST request to authenticate and sign JWT
 authRouter.post('/login', authController.postLogin);
 
 module.exports = authRouter;
