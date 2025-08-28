71130d41008975435de594bb963628a448e4c9b1
man7788 <jerman1841@gmail.com>
2025-08-28 10:21:14 +0800

    update express app

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 require('./config/passport');
 
 app.use('/', routes.auth);
+app.use('/posts', routes.post);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
