e8cde3a0c35d9d309cb5f5bb4b467d21db9ac959
man7788 <jerman1841@gmail.com>
2025-08-29 10:10:58 +0800

    update router index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const auth = require('./authRouter');
 const post = require('./postRouter');
+const comment = require('./commentRouter');
 
 // Barrel file exports
 module.exports = {
   auth,
   post,
+  comment,
 };
