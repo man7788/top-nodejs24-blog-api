c1b5b07da5957f8c4fbd4cb978fad9a6a669a511
man7788 <jerman1841@gmail.com>
2025-08-30 05:03:43 +0800

    create get comment controller

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -17,6 +17,21 @@ exports.postComment = async (req, res) => {
   });
 };
 
+// Handle comment read on GET
+exports.getComment = async (req, res) => {
+  const postId = req.params.postId;
+  const commentId = req.params.commentId;
+
+  const comment = await db.readComment(Number(postId), Number(commentId));
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      comment,
+    },
+  });
+};
+
 // Handle comment update on UPDATE
 exports.patchComment = async (req, res) => {
   const postId = req.params.postId;
