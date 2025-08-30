757e67f340eb857ea3ee89bbc04d774aede6b311
man7788 <jerman1841@gmail.com>
2025-08-30 08:45:29 +0800

    create comment validator

---
+++ utils/validators/commentValidator.js
@@ -0,0 +1,33 @@
+const { body } = require('express-validator');
+
+const emptyErr = 'must not be empty.';
+const formatErr = 'format is not correct.';
+const lengthErr255 = 'must not exceed 255 characters.';
+const lengthErr5to255 = 'must between 5 to 255 characters.';
+
+exports.validateComment = [
+  body('name')
+    .trim()
+    .isLength({ min: 5, max: 255 })
+    .withMessage(`Name ${lengthErr5to255}`)
+    .escape(),
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage(`Email ${emptyErr}`)
+    .bail()
+    .isEmail()
+    .withMessage(`Email ${formatErr}`)
+    .bail()
+    .isLength({ max: 255 })
+    .withMessage(`Email ${lengthErr255}`)
+    .escape(),
+  body('content')
+    .trim()
+    .notEmpty()
+    .withMessage(`Content ${emptyErr}`)
+    .bail()
+    .isLength({ max: 255 })
+    .withMessage(`Content ${lengthErr255}`)
+    .escape(),
+];
