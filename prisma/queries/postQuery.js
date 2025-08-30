8917035b340d78596a50759ef741bba0b0a37dc6
man7788 <jerman1841@gmail.com>
2025-08-31 03:29:50 +0800

    create read all posts query

--- prisma/queries/postQuery.js
+++ prisma/queries/postQuery.js
@@ -13,3 +13,9 @@ exports.createPost = async (author, title, content, published) => {
 
   return created;
 };
+
+// Read all posts
+exports.readAllPosts = async () => {
+  const posts = await prisma.post.findMany({});
+  return posts;
+};
