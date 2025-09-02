fadbc7582adaa73ba64d5bb77b9f6d169df59c3b
man7788 <jerman1841@gmail.com>
2025-09-03 07:26:15 +0800

    update index routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,20 @@ const auth = require('./authRouter');
 const post = require('./postRouter');
 const comment = require('./commentRouter');
 
+const notFound = (req, res, next) => {
+  res.status(404).json({
+    status: 'error',
+    error: {
+      code: 404,
+      message: 'Page not found',
+    },
+  });
+};
+
 // Barrel file exports
 module.exports = {
   auth,
   post,
   comment,
+  notFound,
 };
