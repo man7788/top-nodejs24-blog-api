851e79f22eed668d5e298e2add5523beac2ac5a4
man7788 <jerman1841@gmail.com>
2025-09-03 03:57:23 +0800

    update routes auth

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,24 +1,34 @@
 const { Router } = require('express');
-
+const { customJwtAuth } = require('../utils/passport/customAuth');
 const commentController = require('../controllers/commentController');
 
 const commentRouter = Router();
 
 // POST request to create comment
-commentRouter.post('/:postId/comments', commentController.postComment);
+commentRouter.post(
+  '/:postId/comments',
+  customJwtAuth,
+  commentController.postComment
+);
 
 // GET request to read comment
-commentRouter.get('/:postId/comments/:commentId', commentController.getComment);
+commentRouter.get(
+  '/:postId/comments/:commentId',
+  customJwtAuth,
+  commentController.getComment
+);
 
 // UPDATE request to update comment
 commentRouter.patch(
   '/:postId/comments/:commentId',
+  customJwtAuth,
   commentController.patchComment
 );
 
 // DELETE request to delete comment
 commentRouter.delete(
   '/:postId/comments/:commentId',
+  customJwtAuth,
   commentController.deleteComment
 );
 
