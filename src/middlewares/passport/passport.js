0ad36dbdd8789fce6c5e82542cb2875a1d0810ed
man7788 <jerman1841@gmail.com>
2025-09-11 08:42:05 +0800

    move middlewares to src

--- src/middlewares/passport/passport.js
+++ src/middlewares/passport/passport.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const db = require('../../prisma/queries/userQuery');
+const db = require('../../services/queries/userQuery');
 
 // Convert public key from pem to base64 format
 const publicKey = Buffer.from(process.env.JWT_PUB_KEY, 'base64').toString(
