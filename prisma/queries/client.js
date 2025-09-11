fc2b25137511d7b6fdeadd12307f123dc8a0b1dd
man7788 <jerman1841@gmail.com>
2025-09-11 08:45:22 +0800

    move queries to src

--- prisma/queries/client.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient();
-
-// Export a single client connection for queries
-module.exports = prisma;
---
+++ src/services/queries/client.js
@@ -0,0 +1,17 @@
+const { PrismaClient } = require('@prisma/client');
+
+const databaseUrl =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
+
+// Export a single client connection for queries
+module.exports = prisma;
