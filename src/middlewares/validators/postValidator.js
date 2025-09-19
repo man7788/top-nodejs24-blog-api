cb8df31c4fe7a4a3b561957dc1a8ac698cb7aae1
man7788 <jerman1841@gmail.com>
2025-09-19 11:54:32 +0800

    add post patch validator

--- src/middlewares/validators/postValidator.js
+++ src/middlewares/validators/postValidator.js
@@ -1,6 +1,7 @@
 const { body } = require('express-validator');
 
 const emptyErr = 'must not be empty.';
+const booleanErr = 'must be a boolean value.';
 const lengthErr255 = 'must not exceed 255 characters.';
 
 exports.validatePost = [
@@ -20,4 +21,27 @@ exports.validatePost = [
     .isLength({ max: 255 })
     .withMessage(`Content ${lengthErr255}`)
     .escape(),
+];
+
+exports.validatePatch = [
+  body('title')
+    .trim()
+    .notEmpty()
+    .withMessage(`Title ${emptyErr}`)
+    .bail()
+    .isLength({ max: 255 })
+    .withMessage(`Title ${lengthErr255}`)
+    .escape(),
+  body('content')
+    .trim()
+    .notEmpty()
+    .withMessage(`Content ${emptyErr}`)
+    .bail()
+    .isLength({ max: 255 })
+    .withMessage(`Content ${lengthErr255}`)
+    .escape(),
+  body('published')
+    .isBoolean()
+    .withMessage(`Published ${booleanErr}`)
+    .optional(),
 ];
