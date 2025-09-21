dcefea608702f0fa00148d8ea00cb9b41c732aa8
man7788 <jerman1841@gmail.com>
2025-09-22 01:07:27 +0800

    add post blog post error test

--- tests/unit/controllers/postController.test.js
+++ tests/unit/controllers/postController.test.js
@@ -1,8 +1,57 @@
 const postController = require('../../../src/controllers/postController');
+const { validationResult } = require('express-validator');
 const db = require('../../../src/services/queries/postQuery');
+
+// To mock a named export, use the factory function pattern in jest.mock()
+jest.mock('express-validator', () => ({
+  validationResult: jest.fn(),
+}));
+
+jest.mock('../../../src/middlewares/validators/postValidator', () => ({
+  validatePost: jest.fn(),
+}));
 
 afterEach(async () => {
   jest.clearAllMocks();
+});
+
+describe(`Post blog post controller`, () => {
+  test('response with form validation error', async () => {
+    validationResult.mockImplementation(() => ({
+      isEmpty: () => false,
+      array: () => [
+        { path: 'title', msg: 'Title must not be empty.' },
+        { path: 'content', msg: 'Content must not be empty.' },
+      ],
+    }));
+
+    const mockResponse = () => {
+      const res = {};
+      res.status = jest.fn().mockReturnValue(res);
+      res.json = jest.fn().mockReturnValue(res);
+      return res;
+    };
+
+    const req = { user: { id: 1 } };
+    const res = mockResponse();
+
+    // Second anonymous function of "postBlogPost" controller array
+    await postController.postBlogPost[1](req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({
+      status: 'error',
+      error: {
+        code: 400,
+        message: 'Post create form validation failed.',
+        details: [
+          { field: 'title', message: 'Title must not be empty.' },
+          { field: 'content', message: 'Content must not be empty.' },
+        ],
+      },
+    });
+  });
 });
 
 describe(`Get all posts controller`, () => {
