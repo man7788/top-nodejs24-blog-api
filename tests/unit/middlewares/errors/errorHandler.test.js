ca8bfd876290642c193e1ac54509d7c038aea098
man7788 <jerman1841@gmail.com>
2025-09-26 03:50:18 +0800

    create error handler test

---
+++ tests/unit/middlewares/errors/errorHandler.test.js
@@ -0,0 +1,35 @@
+const {
+  errorHandler,
+} = require('../../../../src/middlewares/errors/errorHandler');
+
+jest.spyOn(console, 'error').mockImplementation(() => {});
+
+describe('Non production environment', () => {
+  test('response with non 500 status code and error message', async () => {
+    const mockResponse = () => {
+      const res = {};
+      res.status = jest.fn().mockReturnValue(res);
+      res.json = jest.fn().mockReturnValue(res);
+      return res;
+    };
+
+    const err = { statusCode: 401, message: 'Error message' };
+    const req = {};
+    const next = jest.fn();
+    const res = mockResponse();
+
+    errorHandler(err, req, res, next);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        status: 'error',
+        error: expect.objectContaining({
+          code: 401,
+          message: 'Error message',
+        }),
+      }),
+    );
+  });
+});
