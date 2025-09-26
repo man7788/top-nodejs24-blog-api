b64a46e610efac9e69d167f156811a32adc50e1b
man7788 <jerman1841@gmail.com>
2025-09-26 09:18:57 +0800

    update get all posts test

--- tests/unit/controllers/postController.test.js
+++ tests/unit/controllers/postController.test.js
@@ -94,17 +94,14 @@ describe(`Post blog post controller`, () => {
 
 describe(`Get all posts controller`, () => {
   test('response with all posts', async () => {
-    const createdAt = new Date();
-    const updateddAt = createdAt;
-
     const posts = [
       {
         id: 1,
         authorId: 1,
         title: 'Post title 1',
         content: 'Post content 1',
-        createdAt,
-        updateddAt,
+        createdAt: new Date(),
+        updateddAt: new Date(),
         published: false,
         comments: [],
       },
@@ -113,8 +110,8 @@ describe(`Get all posts controller`, () => {
         authorId: 1,
         title: 'Post title 2',
         content: 'Post content 2',
-        createdAt,
-        updateddAt,
+        createdAt: new Date(),
+        updateddAt: new Date(),
         published: true,
         comments: [],
       },
