f23f6f4fd037c8fd1053b06c948140dd12b0af9e
man7788 <jerman1841@gmail.com>
2025-09-28 12:46:28 +0800

    add post login password invalid error test

--- tests/integration/routes/authRouter.test.js
+++ tests/integration/routes/authRouter.test.js
@@ -1,6 +1,8 @@
 const prisma = require('../../../src/config/prisma/client');
 const authRouter = require('../../../src/routes/authRouter');
 
+const bcrypt = require('bcryptjs');
+
 const request = require('supertest');
 const express = require('express');
 const app = express();
@@ -8,8 +10,16 @@ const app = express();
 app.use(express.json());
 app.use('/', authRouter);
 
+jest.mock('bcryptjs', () => ({
+  compare: jest.fn(),
+}));
+
+afterEach(async () => {
+  jest.clearAllMocks();
+});
+
 afterAll(async () => {
-  await prisma.user.deleteMany();
+  await prisma.post.deleteMany();
 });
 
 describe(`POST '/login'`, () => {
@@ -52,6 +62,33 @@ describe(`POST '/login'`, () => {
 
   test('response with error if user not found', async () => {
     const payload = { email: 'john@doe.com', password: 'foobar' };
+
+    const response = await request(app)
+      .post('/login')
+      .set('Content-Type', 'application/json')
+      .send(payload);
+
+    expect(response.headers['content-type']).toMatch(/json/);
+    expect(response.status).toEqual(401);
+    expect(response.body).toEqual({
+      status: 'error',
+      error: {
+        code: 401,
+        message: expect.any(String),
+        details: [
+          {
+            field: 'generic',
+            message: expect.any(String),
+          },
+        ],
+      },
+    });
+  });
+
+  test('response with error if password invalid', async () => {
+    bcrypt.compare.mockResolvedValue(false);
+
+    const payload = { email: 'foo@bar.com', password: 'foobar' };
 
     const response = await request(app)
       .post('/login')
