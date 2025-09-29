3fc04b1fdc2762be327c9974e20fa6acaa3a404c
man7788 <jerman1841@gmail.com>
2025-09-30 07:23:42 +0800

    add post login test

--- tests/integration/routes/authRouter.test.js
+++ tests/integration/routes/authRouter.test.js
@@ -2,6 +2,7 @@ const prisma = require('../../../src/config/prisma/client');
 const authRouter = require('../../../src/routes/authRouter');
 
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const request = require('supertest');
 const express = require('express');
@@ -14,12 +15,18 @@ jest.mock('bcryptjs', () => ({
   compare: jest.fn(),
 }));
 
+const bufferSpy = jest.spyOn(Buffer, 'from');
+
+jest.mock('jsonwebtoken', () => ({
+  sign: jest.fn(),
+}));
+
 afterEach(async () => {
   jest.clearAllMocks();
 });
 
 afterAll(async () => {
-  await prisma.post.deleteMany();
+  await prisma.user.deleteMany();
 });
 
 describe(`POST '/login'`, () => {
@@ -108,6 +115,34 @@ describe(`POST '/login'`, () => {
             message: expect.any(String),
           },
         ],
+      },
+    });
+  });
+
+  test('response with jwt token', async () => {
+    bcrypt.compare.mockResolvedValue(true);
+
+    // Use mockImplementationOnce to prevent overriding other use of Buffer.from
+    bufferSpy.mockImplementationOnce(() => ({
+      // Mock the toString method of the returned object
+      toString: jest.fn(() => 'mockedString'),
+    }));
+
+    jwt.sign.mockReturnValue('mockedToken');
+
+    const payload = { email: 'foo@bar.com', password: 'foobar' };
+
+    const response = await request(app)
+      .post('/login')
+      .set('Content-Type', 'application/json')
+      .send(payload);
+
+    expect(response.headers['content-type']).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual({
+      status: 'success',
+      data: {
+        token: expect.any(String),
       },
     });
   });
