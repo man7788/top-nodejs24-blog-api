15efa8515ab56b27974527fae25c9691c5666e51
man7788 <jerman1841@gmail.com>
2025-10-01 11:34:44 +0800

    update delete comment controller

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -129,7 +129,7 @@ exports.deleteComment = async (req, res) => {
     return res.status(404).json({
       status: 'error',
       error: {
-        code: 400,
+        code: 404,
         message: 'Not found',
       },
     });
