db75aacaf23fe316d089d9aab001edba3a6d0ad3
man7788 <jerman1841@gmail.com>
2025-10-01 11:35:15 +0800

    add delete comment not found error

--- tests/unit/controllers/commentController.test.js
+++ tests/unit/controllers/commentController.test.js
@@ -286,3 +286,33 @@ describe(`Patch comment controller`, () => {
     });
   });
 });
+
+describe(`Delete comment controller`, () => {
+  test('response with comment not found error', async () => {
+    db.readComment = jest.fn();
+    db.readComment.mockReturnValue(null);
+
+    const mockResponse = () => {
+      const res = {};
+      res.status = jest.fn().mockReturnValue(res);
+      res.json = jest.fn().mockReturnValue(res);
+      return res;
+    };
+
+    const req = { params: { commentId: 1, postId: 1 } };
+    const res = mockResponse();
+
+    await commentController.deleteComment(req, res);
+
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({
+      status: 'error',
+      error: {
+        code: 404,
+        message: 'Not found',
+      },
+    });
+  });
+});
