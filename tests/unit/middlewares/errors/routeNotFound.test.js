21ce8b15dff870ad396bd26c5f66d3ad283e3c6d
man7788 <jerman1841@gmail.com>
2025-10-05 08:05:01 +0800

    update route not found test

--- tests/unit/middlewares/errors/routeNotFound.test.js
+++ tests/unit/middlewares/errors/routeNotFound.test.js
@@ -1,18 +1,22 @@
 const notFound = require('../../../../src/middlewares/errors/routeNotFound');
 
+beforeEach(async () => {
+  res = {
+    // Allow chaining of .status().json()
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn(),
+    send: jest.fn(),
+  };
+  req = {};
+  next = jest.fn();
+});
+
+afterEach(async () => {
+  jest.clearAllMocks();
+});
+
 describe('Route not found handler', () => {
-  test('response with 400 status code and error message', async () => {
-    const mockResponse = () => {
-      const res = {};
-      res.status = jest.fn().mockReturnValue(res);
-      res.json = jest.fn().mockReturnValue(res);
-      return res;
-    };
-
-    const req = {};
-    const res = mockResponse();
-    const next = jest.fn();
-
+  test('response with 404 status code and error message', async () => {
     notFound(req, res, next);
 
     expect(res.status).toHaveBeenCalledTimes(1);
