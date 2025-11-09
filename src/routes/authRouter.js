9c0ac9b27758f8f489d96938265eee1bbb54eaae
man7788 <jerman1841@gmail.com>
2025-11-09 13:21:38 +0800

    create get auth route

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
-
+const customJwtAuth = require('../middlewares/passport/customAuth');
 const authController = require('../controllers/authController');
 
 const authRouter = Router();
 
 // POST request to authenticate and sign JWT
 authRouter.post('/login', authController.postLogin);
+
+// GET request to vertify JWT
+authRouter.get('/auth', customJwtAuth, authController.getAuth);
 
 module.exports = authRouter;
