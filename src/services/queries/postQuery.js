d2465f37a41318ab1235ab72f3a72e9c73730806
man7788 <jerman1841@gmail.com>
2025-11-12 12:29:07 +0800

    create read all posts (admin)

--- src/services/queries/postQuery.js
+++ src/services/queries/postQuery.js
@@ -14,8 +14,21 @@ exports.createPost = async (author, title, content, published) => {
   return created;
 };
 
-// Read all posts
+// Read all posts (client)
 exports.readAllPosts = async () => {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    omit: { published: true },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return posts;
+};
+
+// Read all posts (admin)
+exports.readAllPostsAdmin = async () => {
   const posts = await prisma.post.findMany({
     orderBy: {
       createdAt: 'desc',
