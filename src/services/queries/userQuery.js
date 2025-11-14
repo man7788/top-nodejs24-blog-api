977edc2e37560f7cf3338c55abb64a9c4c4a0b07
man7788 <jerman1841@gmail.com>
2025-11-14 11:17:56 +0800

    create password queries

--- src/services/queries/userQuery.js
+++ src/services/queries/userQuery.js
@@ -21,11 +21,32 @@ exports.readUserById = async (id) => {
   return user;
 };
 
+// Read user password by ID
+exports.readUserPasswordById = async (id) => {
+  const user = await prisma.user.findUnique({
+    where: { id },
+    select: { password: true },
+  });
+
+  return user.password;
+};
+
 // Update user by ID
 exports.updateUserById = async (id, name) => {
   const updated = await prisma.user.update({
     where: { id },
     data: { name },
+    omit: { password: true },
+  });
+
+  return updated;
+};
+
+// Update user password by ID
+exports.updateUserPasswordById = async (id, password) => {
+  const updated = await prisma.user.update({
+    where: { id },
+    data: { password },
     omit: { password: true },
   });
 
